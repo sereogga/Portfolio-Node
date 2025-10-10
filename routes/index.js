93dc79bea5a10d664f478dbd7b02bcacb2ef5033
Sergio <sereoga24@hotmail.com>
2025-10-10 17:55:41 +0200

    Portfolio refactor

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const path = require("path")
 router.get('/', function(req, res, next) {
   let data = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
   let recommendations = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
-  res.render('index', { title: 'Express' , array: JSON.parse(data), data: JSON.parse(recommendations)});
+  let cakes = fs.readFileSync(path.resolve(__dirname, "../data/portfolio.json"));
+  res.render('index', { title: 'Express' , array: JSON.parse(data), data: JSON.parse(recommendations), cakes: JSON.parse(cakes)});
 });
 module.exports = router;

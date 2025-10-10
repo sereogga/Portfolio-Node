bf2ca1ceb8c5bc217b0598badf6ec6aca74fd9c4
Sergio <sereoga24@hotmail.com>
2025-10-10 11:40:04 +0200

    Add handling delete introduction

--- routes/home.js
+++ routes/home.js
@@ -1,22 +1,44 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const fs = require("fs")
-const path = require("path")
-var bodyParser = require('body-parser')
-var jsonParser = bodyParser.json()
+const fs = require("fs");
+const path = require("path");
+var bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  let data = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
-  res.render('home', { array: JSON.parse(data)});
+router.get("/", function (req, res, next) {
+  let data = fs.readFileSync(
+    path.resolve(__dirname, "../data/introductionArray.json")
+  );
+  res.render("home", { array: JSON.parse(data) });
 });
 /* POST home typed.js page. */
-router.post('/', jsonParser, function(req, res, next) {
-  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
+router.post("/", jsonParser, function (req, res, next) {
+  let rawdata = fs.readFileSync(
+    path.resolve(__dirname, "../data/introductionArray.json")
+  );
   let array = JSON.parse(rawdata);
-  const newArray = array.concat([req.body.newText])
-  fs.writeFileSync(path.resolve(__dirname, "../data/introductionArray.json"), JSON.stringify(newArray));
+  const newArray = array.concat([req.body.newText]);
+  fs.writeFileSync(
+    path.resolve(__dirname, "../data/introductionArray.json"),
+    JSON.stringify(newArray)
+  );
   res.end();
+});
+/* POST home typed.js page. */
+router.delete("/", jsonParser, function (req, res, next) {
+  let rawdata = fs.readFileSync(
+    path.resolve(__dirname, "../data/introductionArray.json")
+  );
+  let array = JSON.parse(rawdata);
+  if (array.includes(req.body.deletedText)) {
+    const newArray = array.filter((text) => text !== req.body.deletedText);
+    fs.writeFileSync(
+      path.resolve(__dirname, "../data/introductionArray.json"),
+      JSON.stringify(newArray)
+    );
+    res.end();
+  }
 });
 
 module.exports = router;
